80eeb8f380633c84e932ca1d661d9c90a49b17cd
Estevao <estevaovasques@hotmail.com>
2024-06-05 22:38:35 -0300

    Testes e mais alguma coisa

--- src/application/product.ts
+++ src/application/product.ts
@@ -1,12 +1,16 @@
 import { v4 as uuidv4 } from 'uuid'
 
+import { Equals, IsIn, IsNumber, IsString, IsUUID, isUUID, validate } from 'class-validator'
+
 type ProductConstructor = {
+  id?: string
   name: string
   price: number
+  status?: string
 }
 
 interface ProductInterface {
-  isValid(): boolean | never;
+  isValid(): Promise<boolean | never>;
   enable(): void | never;
   disable(): void | never
 
@@ -16,38 +20,73 @@ interface ProductInterface {
   getPrice(): number
 }
 
-const Status = {
+export const StatusProduct = {
   DISABLED: 'disabled',
   ENABLED: 'enabled'
-}
+} as const
 
 export class Product implements ProductInterface {
+  @IsUUID()
   id: string
+
+  @IsString()
   name: string
+
+  @IsNumber()
   price: number
+
+  @IsIn([StatusProduct.ENABLED, StatusProduct.DISABLED])
   status: string = ''
 
-  constructor({ name, price }: ProductConstructor) {
-    this.id = uuidv4()
+  constructor({ id, name, status, price }: ProductConstructor) {
+    this.id = id ?? uuidv4()
     this.name = name
     this.price = price
+    this.status = status ?? ''
   }
 
-  isValid(): boolean {
-    throw new Error("Method not implemented.");
+  async isValid(): Promise<boolean | never> {
+    if (this.status == '') {
+      this.status = StatusProduct.DISABLED
+    }
+
+    if (this.status !== StatusProduct.ENABLED && this.status !== StatusProduct.DISABLED) {
+      throw new Error('the status must be enabled or disabled')
+    }
+
+
+    console.log(this.price, this.price < 0)
+    if (this.price < 0) {
+      throw new Error('the price must be greater or equal zero')
+    }
+
+    const errors = await validate(this)
+
+    if (errors.length) {
+      throw new Error('Product has errors validate')
+    }
+
+    return true
   }
 
   enable(): void | never {
-    if (this.price <= 0) {
-      throw new Error('the price must be greater than zero to enable the product')
+    if (this.price > 0) {
+      this.status = StatusProduct.ENABLED
+      return
     }
 
-    this.status = Status.ENABLED
+    throw new Error('the price must be greater than zero to enable the product')
   }
 
-  disable(): void {
-    throw new Error("Method not implemented.");
+  disable(): void | never {
+    if (this.price === 0) {
+      this.status = StatusProduct.DISABLED
+      return
+    }
+
+    throw new Error('the price must be zero in order to have the product disabled')
   }
+
   getId(): string {
     return this.id;
   }
--- src/test/produtc.spec.ts
+++ src/test/produtc.spec.ts
@@ -1,18 +1,60 @@
-import { Product } from "../application/product"
+import { Product, StatusProduct } from "../application/product"
 
 describe('product test', () => {
 
-  it('deve lancar um erro ao habilitar um produto quando preco for 0', () => {
+  it('deve habilitar um produto', () => {
+    const product = new Product({ name: 'Hello', price: 10, })
+    product.enable()
+
+    expect(product.getStatus()).toBe(StatusProduct.ENABLED)
+  })
+
+  it('deve lancar um Erro ao habilitar um produto quando preco for 0', () => {
     expect(() => {
       const product = new Product({ name: 'Teste', price: 0 })
       product.enable()
-    }).toThrow()
+    }).toThrow('the price must be greater than zero to enable the product')
   })
 
-  it('deve habilitar um produto', () => {
-    const product = new Product({ name: 'Teste', price: 10, })
-    product.enable()
+  it('deve desabilitar um produto', () => {
+    const product = new Product({ name: 'Hello', price: 0 })
+    product.disable()
 
-    expect(product.getStatus()).toBe('enabled')
+    expect(product.getStatus()).toBe(StatusProduct.DISABLED)
   })
+
+  it('deve lancar um Erro ao desabilitar um produto quando preco nao for zero', () => {
+    expect(() => {
+      const product = new Product({ name: 'Teste', price: 12 })
+      product.disable()
+    }).toThrow('the price must be zero in order to have the product disabled')
+  })
+
+
+  it('deve lancar um Erro ao verificar o produto usando o isValid de um produto invalido', async () => {
+    let product = new Product({ id: 'dqwwdjkqopijd', name: 'hello', status: StatusProduct.DISABLED, price: 10 })
+    expect(async () => {
+      await product.isValid()
+    }).rejects.toThrow('Product has errors validate')
+
+    product = new Product({ name: 'hello', status: 'estado-', price: 10 })
+    expect(async () => {
+      await product.isValid()
+    }).rejects.toThrow('the status must be enabled or disabled')
+
+
+    product = new Product({ name: 'hello', status: '', price: -10 })
+    expect(async () => {
+      await product.isValid()
+    }).rejects.toThrow('the price must be greater or equal zero')
+  })
+
+  it('deve validar um produto', async () => {
+    const product = new Product({ id: 'ad1c7d38-45fd-438e-b7a6-76e2faaae198', name: 'hello', status: StatusProduct.DISABLED, price: 10 })
+
+    const productIsValid = await product.isValid()
+
+    expect(productIsValid).toBe(true)
+  })
+
 })
